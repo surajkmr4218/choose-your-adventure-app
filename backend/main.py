c184469f36a55748cf6090de1a93efff919e961b
Suraj Kumar <surajkumar@Mac.localdomain>
2025-07-21 21:07:52 -0700

    fastapi setup

--- backend/main.py
+++ backend/main.py
@@ -8,3 +8,17 @@ app = FastAPI(
     docs_url="/docs",
     redoc_url="/redoc"
 )
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
